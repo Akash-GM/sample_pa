8a362ee745c94a67ce89127ab996c27165671393
unknown <george.akash99@gmail.com>
2024-11-29 22:55:59 +0530

    first ever commit , have created the req.txt file ,main script , kept chrome driver in the main project(in gitignore)

---
+++ sample_pa.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+# Initialize the Selenium WebDriver with the path to ChromeDriver in the same directory
+service = Service('./chromedriver.exe')
+driver = webdriver.Chrome(service=service)
+
+# Open Google
+driver.get("https://www.google.com")
+
+
+# Wait for the search results to load
+driver.implicitly_wait(3)  # Adjust the wait time as needed
+
+# Find the search box element by name
+search_box = driver.find_element(By.NAME, "q")
+
+# Enter a search query
+search_query = "Selenium Python"
+search_box.send_keys(search_query)
+
+# Submit the search
+search_box.send_keys(Keys.RETURN)
+
+
+# Extract and print the titles of the search results
+search_results = driver.find_elements(By.XPATH, '//h3')
+for result in search_results:
+    print(result.text)
+
+# Close the browser
+print("closing the driver ...")
+driver.quit()
